30326fda9a18d49e01618aec2cbb204ea9eefc16
MBOGOVIC\mbogovic <mbogovic@croz.net>
2024-01-22 17:57:17 +0100

    Fix import of css

--- src/main/java/hr/unizd/pzw/springmvc/SpringmvcApplication.java
+++ src/main/java/hr/unizd/pzw/springmvc/SpringmvcApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
